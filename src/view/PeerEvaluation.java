ccf9922455d6716c0691880ce51eeb9c4afdcb53
Gideon Paul <gidzpaul95@gmail.com>
2017-12-01 16:46:04 -0500

    Initial screen

--- src/view/PeerEvaluation.java
+++ src/view/PeerEvaluation.java
@@ -4,49 +4,81 @@ import java.awt.event.*;
 
 public class PeerEvaluation
 {
-	public static void main(String args[])
-	{
-		//Create a frame. This is the window.
-		JFrame frame = new JFrame("Peer Evaluation System");
-		frame.setSize(300,150);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		//Add elements to the newly created window
-		addElements(frame);
-	}
-	
-	private static void addElements(JFrame frame)
-	{
-		//Create a panel. Java's equivalent of HTML's div tag
-		JPanel panel = new JPanel();
-		panel.setLayout(null);
-		String[] numbers = { "0","1","2","3","4","5","6","7","8","9","10", };
-		
-		//Create a label
-		JLabel label = new JLabel("Choose the number of team members");
-		label.setBounds(25,20,250,25);
-		
-		//Create the combo box.
-		JComboBox<String> teamMembers = new JComboBox<String>(numbers);
-		teamMembers.setSelectedIndex(0);
-		teamMembers.setBounds(100,50,80,25);
-		
-		//Add to the panel
-		panel.add(label);
-		panel.add(teamMembers);
-		
-		//Add the panel to frame
-		frame.add(panel);
-		frame.setVisible(true);
-		
-				
-		//Add an action listener
-		teamMembers.addActionListener(new ActionListener() {
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			JComboBox cb = (JComboBox)e.getSource();
-			String selection = (String)cb.getSelectedItem();
-		}
-		});
-	}
+    public static void main(String args[])
+    {
+        //Create a frame. This is the window.
+        JFrame frame = new JFrame("Peer Evaluation System");
+        frame.setSize(350,250);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add elements to the newly created window
+        addElements(frame);
+    }
+
+    private static void addElements(JFrame frame)
+    {
+        //Create a panel. Java's equivalent of HTML's div tag
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+        String[] teamMemberOptions = { " ","2","3","4","5","6","7"};
+        String[] scoresEnteredOptions = { " ","Yes","No"};
+
+        //Create a labels
+        JLabel teamMembersLabel = new JLabel("Choose the number of team members");
+        JLabel previousScoresLabel = new JLabel("Are the scores previously entered?");
+
+
+        //Create the combo boxes.
+        JComboBox<String> teamMembers = new JComboBox<String>(teamMemberOptions);
+        teamMembers.setSelectedIndex(0);
+
+        JComboBox<String> scoresEntered = new JComboBox<String>(scoresEnteredOptions);
+        scoresEntered.setSelectedIndex(0);
+
+        //The submit button
+        JButton submitButton = new JButton("Submit");
+
+        //Sizes and Positioning
+        teamMembersLabel.setBounds(25,20,280,25);
+        previousScoresLabel.setBounds(25,80,250,25);
+        teamMembers.setBounds(100,50,80,25);
+        scoresEntered.setBounds(100,110,80,25);
+        submitButton.setBounds(100,150,100,25);
+
+        //Add the components to panel
+        panel.add(teamMembersLabel);
+        panel.add(previousScoresLabel);
+        panel.add(teamMembers);
+        panel.add(scoresEntered);
+        panel.add(submitButton);
+
+        //Add the panel to frame
+        frame.add(panel);
+        frame.setVisible(true);
+
+
+        //Add an action listener
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String teamMembersSelectedItem = (String) teamMembers.getSelectedItem();
+                String scoresEnteredSelectedItem = (String) scoresEntered.getSelectedItem();
+
+                //Checks
+                if(teamMembersSelectedItem.equals(" "))
+                {
+                    System.out.println("Please choose the number of team members.");
+                }
+                else if (scoresEnteredSelectedItem.equals(" "))
+                {
+                    System.out.println("Please choose if the scores were entered previously or not");
+                }
+                else
+                {
+                    //TODO:Proceed to the next window
+                    System.out.println(teamMembersSelectedItem+" "+scoresEnteredSelectedItem);
+                }
+            }
+        });
+    }
 }
