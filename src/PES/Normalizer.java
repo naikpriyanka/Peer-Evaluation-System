7c3026e79d3dd0eeeda68f1f0f15f95fc4c4d850
Anirudh Yellapragada <anirudhyellapragada@gmail.com>
2017-12-03 22:36:51 -0500

    Updated Normalizer.java with comments

--- src/PES/Normalizer.java
+++ src/PES/Normalizer.java
@@ -18,8 +18,11 @@ public class Normalizer {
 
     Map<String,Float> normalize(Map<String,List<Integer>> rawScores)
     {
-        Map<String, Float> result = new HashMap<String, Float>();
+	Map<String, Float> result = new HashMap<String, Float>();
 	float totalScore = 0;
+	    
+	// For each entry(name), the list of scores(three) given by a student are added 
+	// and stored as intermediate score in a map(result)
 	for (Map.Entry<String, List<Integer>> entry : rawScores.entrySet()) {
 		float totalValue = 0;
 		for (Integer score : entry.getValue()) {
@@ -28,6 +31,8 @@ public class Normalizer {
 		totalScore += totalValue;
 		result.put(entry.getKey(), totalValue);
 	}
+	
+	// Finally, the intermediate scores are normalized by dividing with the total score
 	if (totalScore != 0) {
 		for (Map.Entry<String, Float> entry : result.entrySet()) {
 			result.put(entry.getKey(), entry.getValue() / totalScore);
