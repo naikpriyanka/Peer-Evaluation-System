4212e01af13fe090a16eed17b68192522a2394ed
Gideon Paul <gidzpaul95@gmail.com>
2017-12-04 16:46:30 -0500

    Updated the tester file

--- src/PES/tester.sh
+++ src/PES/tester.sh
@@ -1,4 +1,12 @@
-javac *.java;
 mkdir PES;
-mv *.class PES/;
-java PES.PeerEvaluation;
+mkdir PES/util;
+mkdir PES/view;
+
+javac util/*.java;
+mv util/*.class PES/util;
+
+javac -sourcepath PES/util/*.class view/*.java;
+
+mv view/*.class PES/view;
+
+java PES.view.PeerEvaluation;
