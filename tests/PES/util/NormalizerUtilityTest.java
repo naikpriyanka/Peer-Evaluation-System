fdc5b66aa309d0ead99bceebf279fb2197faf43d
hrishikesh-saraf <hrishikesh-saraf@users.noreply.github.com>
2017-12-06 17:09:18 -0500

    Test Cases Written

--- tests/PES/util/NormalizerUtilityTest.java
+++ tests/PES/util/NormalizerUtilityTest.java
@@ -1,11 +1,81 @@
 package PES.util;
 
+import java.util.*;
 import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
 
 public class NormalizerUtilityTest {
+    private NormalizerUtility nu;
 
-    @Test
-    public void normalize() throws Exception {
+    @Before
+    public void setUp(){
+        nu = new NormalizerUtility();
     }
 
+    @Test
+    public void OnePerson() throws Exception {
+    	Map<String, List<Integer>> mymap = new HashMap<>();
+    	mymap.put("Person 1",new ArrayList<>(Arrays.asList(4,3,5)));
+    	Map<String,Float> expectedMap = new HashMap<>();
+    	expectedMap.put("Person 1",1.0f);
+    	assertEquals(expectedMap,nu.normalize(mymap));
+
+    }
+
+    @Test
+    public void TwoPeople() throws Exception {
+        Map<String, List<Integer>> mymap = new HashMap<>();
+        mymap.put("Person 1",new ArrayList<>(Arrays.asList(4,3,5)));
+        mymap.put("Person 2",new ArrayList<>(Arrays.asList(2,0,5)));
+        Map<String,Float> expectedMap = new HashMap<>();
+        expectedMap.put("Person 1",(float)12/19);
+        expectedMap.put("Person 2",(float)7/19);
+        assertEquals(expectedMap,nu.normalize(mymap));
+
+    }
+
+    @Test
+    public void SixPeople() throws Exception {
+        Map<String, List<Integer>> mymap = new HashMap<>();
+        mymap.put("Person 1",new ArrayList<>(Arrays.asList(4,3,5)));
+        mymap.put("Person 2",new ArrayList<>(Arrays.asList(2,0,5)));
+        mymap.put("Person 3",new ArrayList<>(Arrays.asList(1,4,3)));
+        mymap.put("Person 4",new ArrayList<>(Arrays.asList(5,5,5)));
+        mymap.put("Person 5",new ArrayList<>(Arrays.asList(0,0,0)));
+        mymap.put("Person 6",new ArrayList<>(Arrays.asList(2,4,5)));
+        Map<String,Float> expectedMap = new HashMap<>();
+        expectedMap.put("Person 1",(float)12/53);
+        expectedMap.put("Person 2",(float)7/53);
+        expectedMap.put("Person 3",(float)8/53);
+        expectedMap.put("Person 4",(float)15/53);
+        expectedMap.put("Person 5",(float)0/53);
+        expectedMap.put("Person 6",(float)11/53);
+
+        assertEquals(expectedMap,nu.normalize(mymap));
+
+    }
+
+    @Test
+    public void SevenPeople() throws Exception {
+        Map<String, List<Integer>> mymap = new HashMap<>();
+        mymap.put("Person 1",new ArrayList<>(Arrays.asList(4,3,5)));
+        mymap.put("Person 2",new ArrayList<>(Arrays.asList(2,0,5)));
+        mymap.put("Person 3",new ArrayList<>(Arrays.asList(1,4,3)));
+        mymap.put("Person 4",new ArrayList<>(Arrays.asList(5,5,5)));
+        mymap.put("Person 5",new ArrayList<>(Arrays.asList(0,0,0)));
+        mymap.put("Person 6",new ArrayList<>(Arrays.asList(2,4,5)));
+        mymap.put("Person 7",new ArrayList<>(Arrays.asList(3,2,3)));
+        Map<String,Float> expectedMap = new HashMap<>();
+        expectedMap.put("Person 1",(float)12/61);
+        expectedMap.put("Person 2",(float)7/61);
+        expectedMap.put("Person 3",(float)8/61);
+        expectedMap.put("Person 4",(float)15/61);
+        expectedMap.put("Person 5",(float)0/61);
+        expectedMap.put("Person 6",(float)11/61);
+        expectedMap.put("Person 7",(float)8/61);
+
+        assertEquals(expectedMap,nu.normalize(mymap));
+
+    }
 }
